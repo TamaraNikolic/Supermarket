aca9a846e3bccd2753c5884ac14e551788b827aa
Tamara <tamnikolic1501@gmail.com>
2016-09-09 10:53:14 +0200

    Drugi cas nastavak login i registracije

--- Supermarket/app/src/main/java/supermarket/main/LoginActivity.java
+++ Supermarket/app/src/main/java/supermarket/main/LoginActivity.java
@@ -1,9 +1,25 @@
 package supermarket.main;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.SyncStateContract;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,13 +33,16 @@ public class LoginActivity extends AppCompatActivity {
     private TabLayout mTabLayout;
     private PagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
+    private ImageView mIvAdd;
+    private ImageView mIvPicture;
+    private ImageView mIvSupermarket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-   initComponents();
+        initComponents();
         addListneres();
 
 
@@ -33,17 +52,28 @@ public class LoginActivity extends AppCompatActivity {
         mTabLayout.addTab(mTabLayout.newTab().setText(R.string.login));
         mTabLayout.addTab(mTabLayout.newTab().setText(R.string.registration));
         mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        mIvSupermarket=(ImageView)findViewById(R.id.imageViewSupermarket);
 
-
+        mIvAdd = (ImageView) findViewById(R.id.imageViewAdd);
+        mIvPicture=(ImageView)findViewById(R.id.imageViewPicture);
         mPagerAdapter=new PagerAdapter(getSupportFragmentManager(),mTabLayout.getTabCount());
         mViewPager = (ViewPager) findViewById(R.id.pager);
 
         mViewPager.setAdapter(mPagerAdapter);
+
+
+
+
     }
     public void addListneres(){
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
 
-
+mIvAdd.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        selectImage();
+    }
+});
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -51,8 +81,14 @@ public class LoginActivity extends AppCompatActivity {
                 mViewPager.setCurrentItem(tab.getPosition());
                 if (tab.getPosition() == 0) {
 
-                } else if (tab.getPosition() == 1) {
+                    mIvAdd.setVisibility(View.GONE);
+                    mIvPicture.setVisibility(View.GONE);
+                    mIvSupermarket.setVisibility(View.VISIBLE);
 
+                } else if (tab.getPosition() == 1) {
+                    mIvAdd.setVisibility(View.VISIBLE);
+                    mIvPicture.setVisibility(View.VISIBLE);
+                    mIvSupermarket.setVisibility(View.GONE);
 
                 }
             }
@@ -84,5 +120,87 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+    private void selectImage() {
+
+        final CharSequence[] items = { "Take Photo", "Choose from Galery",
+                "Cancel" };
+
+        TextView title = new TextView(getApplicationContext());
+        title.setText("Add Photo!");
+        title.setBackgroundColor(Color.BLACK);
+        title.setPadding(10, 15, 15, 10);
+        title.setGravity(Gravity.CENTER);
+        title.setTextColor(Color.WHITE);
+        title.setTextSize(22);
+
+        ImageView image=new ImageView(getApplicationContext());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                LoginActivity.this);
+
+
+
+        builder.setCustomTitle(title);
+
+        // builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    // Intent intent = new
+                    // Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                /*
+                 * File photo = new
+                 * File(Environment.getExternalStorageDirectory(),
+                 * "Pic.jpg"); intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                 * Uri.fromFile(photo)); imageUri = Uri.fromFile(photo);
+                 */
+                    // startActivityForResult(intent,TAKE_PICTURE);
+
+                    Intent intents = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+
+                } else if (items[item].equals("Choose from Galery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    Glide
+                            .with(getApplicationContext())
+                            .load(selectedImage).bitmapTransform(new CropCircleTransformation(getApplicationContext()))
+                            .into(mIvPicture);
+
+
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    Glide
+                            .with(getApplicationContext())
+                            .load(selectedImage).bitmapTransform(new CropCircleTransformation(getApplicationContext()))
+                            .into(mIvPicture);;
+                }
+                break;
+        }
     }
 }
--- Supermarket/app/src/main/java/supermarket/main/LoginFragment.java
+++ Supermarket/app/src/main/java/supermarket/main/LoginFragment.java
@@ -6,16 +6,25 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by cubesschool3 on 9/7/16.
  */
 public class LoginFragment extends android.support.v4.app.Fragment {
+    private ImageView mIvPicture;
+    private ImageView mIvAdd;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mIvAdd=(ImageView)view.findViewById(R.id.imageViewAdd);
+        mIvPicture=(ImageView)view.findViewById(R.id.imageViewPicture);
+
+
         return view;
     }
 
